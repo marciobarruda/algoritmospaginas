ded37d61c89554847d60e5b48620ca657c64a329
frotajunior16 <frotajunior16@e86c1e64-40a3-11de-9a54-b9394989f659>
2009-05-26 17:41:32 +0000

    Realização de teste

---  algoritmospaginas/apm/src/modelo/Teste.java
+++  algoritmospaginas/apm/src/modelo/Teste.java
@@ -2,6 +2,7 @@ package modelo;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.Vector;
 
 import ngc.*;
@@ -12,13 +13,13 @@ public class Teste {
 		Vector<PaginaRequisitada> pilha = new Vector<PaginaRequisitada>();
 		
 		
-		Referencia r = null;
+		Referencia referencia = null;
 		try {
-			r = new Referencia("c:/REFERENCIAS_10000.txt");
-			int size = r.getListCaract().size();
+			referencia = new Referencia("c:/Nova pasta/REFERENCIAS_10000.txt");
+			int size = referencia.getListCaract().size();
 			
 			for (int i =0;i<size; i++){
-				pilha.add(new PaginaRequisitada(r.getListNum().get(i),r.getListCaract().get(i)));
+				pilha.add(new PaginaRequisitada(referencia.getListNum().get(i),referencia.getListCaract().get(i)));
 			}
 			
 		}catch (FileNotFoundException e) {
@@ -30,21 +31,39 @@ public class Teste {
 			
 			e.printStackTrace();
 		}
-	
-		Alocacao m = new SegundaChance(4);
+		Scanner s = new Scanner(System.in);
+		System.out.println("-----Informe a quantidade de Frames-----");
+		System.out.println("Quantidade minina: ");
+		int min = s.nextInt();
+		System.out.println("Quantidade máxima: ");
+		int max = s.nextInt();
+		for(int i = min; i <= max; i++){
+			
+			Alocacao m = new MRU(i);
+			
+			for(PaginaRequisitada p : pilha) {
+				m.inserir(p);
+			}
+			System.out.println("=-=-=-=-=-=-=MRU=-=-=-=-=-=-=");
+			System.out.println("Quantidade de Frames: "+i+"\n");
+			System.out.println("Acertos: "+m.getAcertos());
+			System.out.println("Falhas: "+m.getFalhas());
+			System.out.println("Porcetagem: "+m.getPorcentagem());
+			System.out.println();
+			
+			
+			m = new SegundaChance(i);
+			
+			for(PaginaRequisitada p : pilha) {
+				m.inserir(p);
+			}
+			System.out.println("=-=-=-=-=-=-=Segunda Chance=-=-=-=-=-=-=");
+			System.out.println("Quantidade de Frames: "+i+"\n");
+			System.out.println("Acertos: "+m.getAcertos());
+			System.out.println("Falhas: "+m.getFalhas());
+			System.out.println("Porcetagem: "+m.getPorcentagem()+"\n");
 		
-		for(PaginaRequisitada p : pilha) {
-			m.inserir(p);
 		}
-		
-		System.out.println(m.getAcertos());
-		System.out.println(m.getFalhas());
-		System.out.println(m.getPorcentagem());
-		/*for(PaginaRequisitada p : m.getPilha()) {
-			System.out.println(p.getNumero());
-		}*/
-		
-		
 	
 	}
 }
