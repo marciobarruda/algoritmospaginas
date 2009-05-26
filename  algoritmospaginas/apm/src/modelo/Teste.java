37d095606baf95c06995bd2995831e0ec9160181
frotajunior16 <frotajunior16@e86c1e64-40a3-11de-9a54-b9394989f659>
2009-05-26 18:13:34 +0000

    Realização de teste

---  algoritmospaginas/apm/src/modelo/Teste.java
+++  algoritmospaginas/apm/src/modelo/Teste.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.Vector;
 
+import javax.swing.JOptionPane;
+
 import ngc.*;
 
 public class Teste {
@@ -15,14 +17,53 @@ public class Teste {
 		
 		Referencia referencia = null;
 		try {
-			referencia = new Referencia("c:/Nova pasta/REFERENCIAS_10000.txt");
+			referencia = new Referencia("c:/Nova pasta/REFERENCIAS_20.txt");
 			int size = referencia.getListCaract().size();
 			
 			for (int i =0;i<size; i++){
 				pilha.add(new PaginaRequisitada(referencia.getListNum().get(i),referencia.getListCaract().get(i)));
 			}
 			
+			Scanner s = new Scanner(System.in);
+			System.out.println("-----Informe a quantidade de Frames-----");
+			System.out.println("Quantidade minina: ");
+			int min = s.nextInt();
+			System.out.println("Quantidade máxima: ");
+			int max = s.nextInt();
+			for(int i = min; i <= max; i++){
+				
+				Alocacao m = new MRU(i);
+				
+				for(PaginaRequisitada p : pilha) {
+					m.inserir(p);
+				}
+				System.out.println("=-=-=-=-=-=-=MRU=-=-=-=-=-=-=");
+				System.out.println("Quantidade de Frames: "+i+"\n");
+				System.out.println("Acertos: "+m.getAcertos());
+				System.out.println("Falhas: "+m.getFalhas());
+				System.out.println("Porcetagem: "+m.getPorcentagem()*100+"%");
+				System.out.println();
+				
+				
+				m = new SegundaChance(i);
+				
+				for(PaginaRequisitada p : pilha) {
+					m.inserir(p);
+				}
+				System.out.println("=-=-=-=-=-=-=Segunda Chance=-=-=-=-=-=-=");
+				System.out.println("Quantidade de Frames: "+i+"\n");
+				System.out.println("Acertos: "+m.getAcertos());
+				System.out.println("Falhas: "+m.getFalhas());
+				System.out.println("Porcetagem: "+m.getPorcentagem()*100+"%\n");
+			
+			}
+			
+			
+			
+			
+			
 		}catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
 			System.out.println(e.getMessage());
 		
 		
@@ -31,39 +72,7 @@ public class Teste {
 			
 			e.printStackTrace();
 		}
-		Scanner s = new Scanner(System.in);
-		System.out.println("-----Informe a quantidade de Frames-----");
-		System.out.println("Quantidade minina: ");
-		int min = s.nextInt();
-		System.out.println("Quantidade máxima: ");
-		int max = s.nextInt();
-		for(int i = min; i <= max; i++){
-			
-			Alocacao m = new MRU(i);
-			
-			for(PaginaRequisitada p : pilha) {
-				m.inserir(p);
-			}
-			System.out.println("=-=-=-=-=-=-=MRU=-=-=-=-=-=-=");
-			System.out.println("Quantidade de Frames: "+i+"\n");
-			System.out.println("Acertos: "+m.getAcertos());
-			System.out.println("Falhas: "+m.getFalhas());
-			System.out.println("Porcetagem: "+m.getPorcentagem());
-			System.out.println();
-			
-			
-			m = new SegundaChance(i);
-			
-			for(PaginaRequisitada p : pilha) {
-				m.inserir(p);
-			}
-			System.out.println("=-=-=-=-=-=-=Segunda Chance=-=-=-=-=-=-=");
-			System.out.println("Quantidade de Frames: "+i+"\n");
-			System.out.println("Acertos: "+m.getAcertos());
-			System.out.println("Falhas: "+m.getFalhas());
-			System.out.println("Porcetagem: "+m.getPorcentagem()+"\n");
 		
-		}
 	
 	}
 }
